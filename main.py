18f00f1c066a24c0efc9a525b363fdd8ce797720
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-22 01:48:37 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -16,5 +16,12 @@ with col2:
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
     It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
     remaining essentially unchanged."""
     st.info(content)
+
+
+content2="""Below you can find some of the apps built in python. Feel free to contact me."""
+st.write(content2)
